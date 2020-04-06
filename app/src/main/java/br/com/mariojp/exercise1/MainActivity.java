e6a52d27b6269b42df2ed41a470a17ff0808a46a
Tiala <tialarocha@Tialas-MacBook-Pro.local>
2020-04-06 14:31:00 -0300

    Atividade 1 completa

--- app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
+++ app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
@@ -5,16 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editNome);
+        button = findViewById(R.id.btnCumprimentar);
+        textView = findViewById(R.id.labelMensagem);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                textView.setText("Alô, " + name + "!");
+            }
+        });
     }
 
 }
